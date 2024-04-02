39a4755d5f17ba03a4073a9166b70b62e67f5730
parnkot <parnkot@Ubuntu.myguest.virtualbox.org>
2024-04-02 22:32:25 +0300

    Start doing

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <type_traits>
+#include <vector>
+#include <list>
+#include <string>
+
+
+template<typename T, std::enable_if<std::is_same_v<T, std::string> || std::is_same_v<std::remove_const<T>, char*>> = nullptr>
+void print_ip(T ip) {
+    std::cout << ip << std::endl;
+}
+
+template<typename T,  size_t N = sizeof(T), std::enable_if<std::is_integral_v<T>> = nullptr>
+void print_ip(T number) {
+    for (int i = N - 1; i >= 0; --i) {
+        uint8_t byte = (number >> i * 8);
+        std::cout << std::to_string(byte) << ".";
+    }
+    std::cout << "\b " << std::endl;
+}
+
+
+int main(int argc, char* argv[]) {
+    print_ip(int8_t{-1});
+    print_ip(int16_t{0});
+    print_ip(int32_t{2130706433});
+    print_ip(int64_t{8875824491850138409});
+    print_ip("Hello world!");
+    
+    return 0;
+}
