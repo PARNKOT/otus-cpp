d8cb784d96ff8d60dad4254a7cd8512a2d35fb58
parnkot <parnkot@Ubuntu.myguest.virtualbox.org>
2024-04-13 15:48:27 +0300

    Initial commit for 05_homework

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include "version.hpp"
+
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
