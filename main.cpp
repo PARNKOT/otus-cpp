00967264f3c15dceed922b43e9f2af461eea3c57
parnkot <parnkot@Ubuntu.myguest.virtualbox.org>
2024-04-30 14:06:30 +0300

    Implement sparse matrix v2

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ namespace matrix = matrix_v2;
 
 int main(int argc, char const *argv[])
 {
-    matrix::SparseArray<int, -1> sparse_array;
-    std::cout << "Sparse array size: " << sparse_array.size() << std::endl;
-    std::cout << "Sparse array [0] = " << sparse_array[0] << std::endl;
-    sparse_array[0] = 2;
-    std::cout << "Sparse array size: " << sparse_array.size() << std::endl;
-    std::cout << "Sparse array [0] = " << sparse_array[0] << std::endl;
+    // matrix::SparseArray<int, -1> sparse_array;
+    // std::cout << "Sparse array size: " << sparse_array.size() << std::endl;
+    // std::cout << "Sparse array [0] = " << sparse_array[0] << std::endl;
+    // sparse_array[0] = 2;
+    // std::cout << "Sparse array size: " << sparse_array.size() << std::endl;
+    // std::cout << "Sparse array [0] = " << sparse_array[0] << std::endl;
 
 
     matrix::SparseMatrix<int, -1> m;
--- matrix_v2.hpp
+++ matrix_v2.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <map>
+#include <type_traits>
 
 namespace matrix_v2 {
     using index_t = std::size_t;
@@ -54,38 +55,59 @@ namespace matrix_v2 {
 
     template<typename  T, T Default>
     struct SparseMatrix {
+        using map_t = std::map<index_t, std::map<index_t, T>>;
+
+        template <typename value_t>
         struct Value {
-            explicit Value(map_t* p_map, index_t index) : map_{p_map}, index_{index} {}
+            explicit Value(map_t* p_map, index_t first_index, index_t second_index) : 
+            parent_map_{p_map}, first_index_{first_index}, second_index_{second_index} {}
 
             Value& operator =(const T& value) {
-                (*map_)[index_] = value;
+                if (parent_map_->count(first_index_) == 0) {
+                    (*parent_map_)[first_index_] = std::map<index_t, T>();
+                }
+
+                parent_map_->at(first_index_)[second_index_] = value;
+
                 return *this;
             }
 
-            operator T() {
-                if (map_->count(index_) == 0) {
+            template <typename Fake = std::enable_if<std::is_same<value_t, T>::value>>
+            operator value_t() {
+                if (parent_map_->count(first_index_)  == 0) {
                     return Default;
                 }
-                return map_->at(index_);
+
+                if (parent_map_->at(first_index_).count(second_index_) == 0) {
+                    return Default;
+                }
+
+                return parent_map_->at(first_index_).at(second_index_);
             };
 
-            T& operator [](index_t index) {
-                if (map_->count(index_) == 0) {
-                    return Default;
-                }
-                return map_->at(index_);
+            template <typename Fake = std::enable_if<std::is_same<value_t, Value<T>>::value>>
+            value_t operator [](index_t index) {
+                return value_t{parent_map_, first_index_, index};
             }
 
             private:
-                void create() {
-                    (*map_)[index_] = value;
-                }
-
-                map_t* map_ = nullptr;
-                index_t index_ = -1;
-                Value* prev = nullptr;
-                Value* next = nullptr;
+                map_t* parent_map_ = nullptr;
+                index_t first_index_ = -1;
+                index_t second_index_ = -1;
         };
+
+        constexpr std::size_t size() const noexcept {
+            std::size_t res{0};
+            for (const auto& p : matrix_){
+                res += p.second.size();
+            }
+
+            return res;
+        }
+
+        Value<Value<T>> operator [](index_t index) {
+            return Value<Value<T>>{&matrix_, index, 0};
+        }
 
     private:
         std::map<index_t, std::map<index_t, T>> matrix_;
