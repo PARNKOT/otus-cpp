b496f38454847ed7f6a6a9e8b3acdfc702fa7e5c
parnkot <parnkot@area.zov>
2024-08-13 14:10:44 +0300

    Added boost options

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <memory>
 #include <yaml-cpp/yaml.h>
+#include <boost/program_options.hpp>
+
 #include "version.hpp"
 #include "YamlReader.hpp"
 #include "CppGenerator.hpp"
@@ -31,12 +33,59 @@ bool save_generated_code(std::string_view code, const fs::path& path) {
 }
 
 int main(int argc, char* argv[])  {
-    std::shared_ptr<ProtoReader> reader = std::make_shared<YamlReader>();
+    // Parsing options
+    namespace po = boost::program_options;
+
+    std::string proto_file;
+    std::string save_to;
+    std::string lang;
+
+    po::options_description desc("Options");
+
+    desc.add_options()
+        ("help", "Usage: ./program [options] -p <proto_file>")
+        ("proto,p", po::value<std::string>(&proto_file), "Protocol file, [yaml only]")
+        ("save,s", po::value<std::string>(&save_to)->default_value("./generated"), "Directory to save")
+        ("lang,l", po::value<std::string>(&lang)->default_value("cpp"), "Programming language to generate")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 0;
+    }
+
+    std::shared_ptr<ProtoReader> reader;
+
+    std::string extension = fs::path(proto_file).extension();
+    if (extension == ".yaml" || extension == ".yml") {
+        reader = std::make_shared<YamlReader>();
+    } else {
+        std::cout << "Unknown extension of protocol file: " << extension << std::endl;
+        return -1;
+    }
+
+    std::unique_ptr<CodeGenerator> generator;
+
+
+    if (lang == "cpp" || lang == "c++") {
+        generator = std::make_unique<CppGenerator>("./templates/cpp_h.txt");
+    } else {
+        std::cout << "Unknown programming language: " << lang << std::endl;
+        return -1;
+    }
+
+    if (!fs::exists(save_to)) {
+        fs::create_directories(save_to);
+    }
 
     UserStructArray struct_array;
 
     try {
-        struct_array = reader->read("./proto/test.yaml");
+        struct_array = reader->read(proto_file);
     } catch (const YAML::BadFile& ex) {
         std::cout << "Cannot read proto file" << std::endl;
         std::cout << ex.what() << std::endl;
@@ -44,16 +93,9 @@ int main(int argc, char* argv[])  {
 
     print_struct_array(struct_array);
 
-    std::unique_ptr<CodeGenerator> generator = std::make_unique<CppGenerator>("./templates/cpp_h.txt");
-
-    constexpr char* generate_to_dir = "./generated";
-    if (!fs::exists(generate_to_dir)) {
-        fs::create_directories(generate_to_dir);
-    }
-
     for (const auto& s : struct_array) {
         auto str = generator->generate(s);
-        save_generated_code(str, fs::path(generate_to_dir) / fs::path(s.name() + ".hpp"));
+        save_generated_code(str, fs::path(save_to) / fs::path(s.name() + ".hpp"));
     }
 
     return 0;
